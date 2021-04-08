f9de2c53b478284dea6a6ae32c46a0555fc35fba
Canturilsnaik <italo.cantur@gmail.com>
2021-04-08 11:13:41 -0300

    Feat: Cors

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
 app.use(cors());
+
+app.use(express.json());
 
 app.use(routes);
 
